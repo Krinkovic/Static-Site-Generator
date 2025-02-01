ea7d04778cc24cd62796c5d5a16497721f577d9b
Krinkovic <kristoffer90forsberg@gmail.com>
2025-02-01 19:21:41 +0000

    fixat programmet efter jag insåg att jag gjort fel gämfört med vad boot.dev förväntade sig

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -4,34 +4,24 @@ from textnode import *
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
         else:
-            text = node.text
-            subnodes = text.split(delimiter, 2)
-            if len(subnodes) < 3:
+            sections = old_node.text.split(delimiter)
+            if len(sections) % 2 == 0:
                 raise Exception("Invalid syntax. No closing delimiter")
-            subnodes[0] = TextNode(subnodes[0], text_type)
-            subnodes[2] = TextNode(subnodes[2], text_type)
-            match delimiter:
-                case "*":
-                    subnodes[1] = TextNode(subnodes[1], TextType.ITALIC)
-                case "**":
-                    subnodes[1] = TextNode(subnodes[1], TextType.BOLD)
-                case "`":
-                    subnodes[1] = TextNode(subnodes[1], TextType.CODE)
-                case _:
-                    pass
-            new_nodes.extend(subnodes)
+            for i in range(len(sections)):
+                if sections[i] == "":
+                    continue
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(sections[i], text_type.TEXT))
+                else:
+                    new_nodes.append(TextNode(sections[i], text_type))
     return new_nodes
 
 def main():
-    node1 = TextNode("This is some **test text**", TextType.TEXT)
-    node2 = TextNode("This is some **test** text", TextType.BOLD)
-    nodes = [node1, node2]
-    new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    print(new_nodes)
+    pass
 
 if __name__ == "__main__":
     main()
--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -5,21 +5,41 @@ from split_delimiter import split_nodes_delimiter
 class TestTextNode(unittest.TestCase):
     def test_text(self):
         node1 = TextNode("This is some **regular** text", TextType.BOLD)
-        nodes = [node1]
-        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter([node1], "**", TextType.BOLD)
         self.assertEqual(new_nodes, [TextNode('This is some **regular** text', TextType.BOLD)])
     
     def test_bold(self):
         node1 = TextNode("This is some **test** text", TextType.TEXT)
-        nodes = [node1]
-        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter([node1], "**", TextType.BOLD)
         self.assertEqual(new_nodes, [TextNode('This is some ', TextType.TEXT), TextNode('test', TextType.BOLD), TextNode(' text', TextType.TEXT)])
+
+    def test_double_bold(self):
+        node1 = TextNode("This is some **test** text, and som more **test** text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node1], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [TextNode('This is some ', TextType.TEXT), TextNode('test', TextType.BOLD), TextNode(' text, and som more ', TextType.TEXT), TextNode('test', TextType.BOLD), TextNode(' text', TextType.TEXT)])
 
     def test_italics(self):
         node1 = TextNode("This is some *test* text", TextType.TEXT)
-        nodes = [node1]
-        new_nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+        new_nodes = split_nodes_delimiter([node1], "*", TextType.ITALIC)
         self.assertEqual(new_nodes, [TextNode('This is some ', TextType.TEXT), TextNode('test', TextType.ITALIC), TextNode(' text', TextType.TEXT)])
+
+    def test_code(self):
+        node1 = TextNode("This is some `test` text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node1], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [TextNode('This is some ', TextType.TEXT), TextNode('test', TextType.CODE), TextNode(' text', TextType.TEXT)])
+
+    def test_multi_type(self):
+        node1 = TextNode("This *is* some **test** text", TextType.TEXT)
+        nodes = split_nodes_delimiter([node1], "**", TextType.BOLD)
+        nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+        self.assertEqual(nodes, [TextNode('This ', TextType.TEXT, None), TextNode('is', TextType.ITALIC, None), TextNode(' some ', TextType.TEXT, None), TextNode('test', TextType.BOLD, None), TextNode(' text', TextType.TEXT, None)])
+
+    def test_not_closed(self):
+        node1 = TextNode("This is some *test text", TextType.TEXT)
+        try:
+            split_nodes_delimiter([node1], "*", TextType.ITALIC)
+        except Exception as e:
+            self.assertEqual("Invalid syntax. No closing delimiter", str(e))
 
 
 if __name__ == "__main__":
