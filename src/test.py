2688449ed232477b76c34de4a14196c375c51d44
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2025-05-30 01:10:11 +0200

    progress on splitting out images, need to reinsert them in the lists.

--- src/test.py
+++ src/test.py
@@ -2,3 +2,5 @@ from inline_markdown import *
 
 text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm3Vk.jpeg)"
 print(extract_markdown_images(text))
+text = 'heso'
+print(text.split('es'))
